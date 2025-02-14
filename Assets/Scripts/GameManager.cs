a9e82b09e20f849410db830962df1076b0c3c104
hair <doxuanhai223@gmail.com>
2025-02-14 11:45:03 +0700

    DATN

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,9 @@ public class GameManager : MonoBehaviour
     [Header("Checkpoints")]
     public bool canReactivate;
 
+    [Header("Traps")]
+    public GameObject arrowPrefeb;
+
     private void Awake()
     {
         if (instance == null)
@@ -54,4 +57,16 @@ public class GameManager : MonoBehaviour
     }
     public void AddFruit()  => fruitsCollected++;
     public bool FruitsHaveRandomLook() => fruitsAreRandom;
+
+    public void CreateObject(GameObject prefab, Transform target, float delay = 0)
+    {
+        StartCoroutine(CreateObjectCoroutine(prefab, target, delay));
+    }
+
+    private IEnumerator CreateObjectCoroutine(GameObject prefab, Transform target, float delay)
+    {
+        Vector3 newPosition = target.position;
+        yield return new WaitForSeconds(delay);
+        GameObject newObject = Instantiate(prefab, newPosition, Quaternion.identity);   
+    }
 }
